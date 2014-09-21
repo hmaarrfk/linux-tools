463d7e3cdf24e066e25d71761ad02f919ed29b94
Mark Harfouche <mark.harfouche@gmail.com>
2014-09-21 16:53:39 -0700

    Added a super simple install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Super simple script, It will not overwrite any config files that already
+# exist on your system
+
+ln -s ~/bash-tools/vim ~/.vim
+ln -s .vim/vimrc ~/.vimrc
+ln -s .vim/gvimrc ~/.gvimrc
+
+ln -s ~/bash-tools/bash/bash_aliases ~/.bash_aliases
+ln -s ~/bash-tools/bash/bash_logout ~/.bash_logout
+ln -s ~/bash-tools/bash/bash_profile ~/.bash_profile
+ln -s ~/bash-tools/bash/bashrc ~/.bashrc
+
+ln -s ~/bash-tools/git/gitconfig ~/.gitconfig
+ln -s ~/bash-tools/git/cvsignore ~/.cvsignore
