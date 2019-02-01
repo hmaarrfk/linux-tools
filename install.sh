5ab516afd412dfcd5c6f77ff3dc9cbea9c00d82b
Mark Harfouche <mark.harfouche@gmail.com>
2019-01-31 19:56:57 -0500

    Added my xonsh rc file

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # Super simple script, It will not overwrite any config files that already
 # exist on your system
 
-stow vim
-stow bash
-stow git
+stow "$@" vim
+stow "$@" bash
+stow "$@" git
+stow "$@" xonsh
